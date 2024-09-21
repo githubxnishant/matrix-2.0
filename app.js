c294b933ba60d4ab6c462331421ddd32ddcda53c
Nishant Chauhan <mailxnishant@gmail.com>
2024-09-21 11:32:32 +0530

    all commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const button = document.getElementById('userId');
+    
+        button.addEventListener('click', function(){
+            const userInput = prompt("Enter new button text:");
+            if (userInput !== null && userInput.trim() !== "") {
+                button.textContent = userInput;
+            }
+        });
+    });
+    
